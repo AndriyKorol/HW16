e2e16bff8094233758745ae23fb7b693f91d4252
Andriy <napuwunapuwu@gmail.com>
2018-07-20 17:57:06 +0300

    HW16

--- js/login.js
+++ js/login.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //auth init
 const auth = new Auth();
 // Init UI
@@ -36,42 +35,3 @@ function onLogin(e) {
 function signUp() {
     window.location = 'sign-up.html'
 }
-=======
-//auth init
-const auth = new Auth();
-// Init UI
-const ui = new UI();
-
-//init elements from form
-const form = document.forms['login-form'];
-const email = document.getElementById("email");
-const password = document.getElementById('password');
-const signup = document.querySelector('.signup');
-
-//check auth state (is it user logged)
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        window.location = "index.html";
-    }
-});
-
-form.addEventListener("submit", onLogin);
-signup.addEventListener("click", signUp);
-
-function onLogin(e) {
-    e.preventDefault();
-
-    if (email.value && password.value || !email.value || !password.value){
-        auth.login(email.value, password.value)
-            .then(() => {
-                window.location = "index.html";
-            })
-            .catch((err) => { return ui.formError(err) });
-    }
-    document.forms['login-form'].reset();
-}
-
-function signUp() {
-    window.location = 'sign-up.html'
-}
->>>>>>> a3e23470f25f907f94c6d47b9d01040bbcb6d97a
--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //auth init
 const auth = new Auth();
 // Init UI
@@ -38,43 +37,4 @@ function newUser(e) {
 
 function goLogin() {
     window.location = 'login-start.html'
-=======
-//auth init
-const auth = new Auth();
-// Init UI
-const ui = new UI();
-
-//init elements from form
-const signupform = document.forms['signup-form'];
-const userName = document.getElementById("username");
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const loginBtn = document.querySelector('.login');
-
-//check auth state (is it user logged)
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        window.location = "index.html";
-    }
-});
-
-signupform.addEventListener("submit", newUser);
-loginBtn.addEventListener("click", goLogin);
-
-function newUser(e) {
-    e.preventDefault();
-
-    if (userName.value && email.value && password.value || !email.value || !password.value || !userName.value){
-        auth.signup(userName.value, email.value, password.value)
-            .then(() => {
-                window.location = "index.html";
-            })
-            .catch((err) => { return ui.formError(err) })
-    }
-    document.forms['signup-form'].reset();
-}
-
-function goLogin() {
-    window.location = 'login-start.html'
->>>>>>> a3e23470f25f907f94c6d47b9d01040bbcb6d97a
 }
